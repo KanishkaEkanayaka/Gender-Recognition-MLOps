fa2dc776ef4000e0c412de6d63aecc41f7f08fa7
KanishkaEkanayaka <kanishkaekanayaka1004@outlook.com>
2024-05-08 00:36:50 +0530

    add evaluation pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.cnnGenderClassifier.utils import logger
 from src.cnnGenderClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnGenderClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.cnnGenderClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+from src.cnnGenderClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -32,6 +33,18 @@ try:
     logger.info(f"******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Evaluation stage"
+
+try:
+    logger.info(f"******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
--- src/cnnGenderClassifier/config/configuration.py
+++ src/cnnGenderClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from cnnGenderClassifier.constants import *
 from cnnGenderClassifier.utils.common import read_yaml, create_directories
-from cnnGenderClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig,TrainingConfig
+from cnnGenderClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig,TrainingConfig, EvaluationConfig
 import os
 
 class ConfigurationManager:
@@ -70,3 +70,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model='artifacts/training/model.keras',
+            training_data='artifacts/data_ingestion/Gender',
+            mlflow_uri="https://dagshub.com/KanishkaEkanayaka/Gender-Recognition-MLOps.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
