999b916e2cec5f25a20a1a2f1940ca59efea7800
KanishkaEkanayaka <kanishkaekanayaka1004@outlook.com>
2024-05-07 20:38:58 +0530

    add training stage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.cnnGenderClassifier.utils import logger
 from src.cnnGenderClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnGenderClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnGenderClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -19,6 +20,18 @@ try:
     logger.info(f"******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f"******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
